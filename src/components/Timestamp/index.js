5ab47d8856bc0586236a7afecdf5d7333ded5695
Edson Lucas <front1.shopack@gmail.com>
2021-11-15 17:41:05 -0300

    adjusting the styles of the timesTamp component and fixing the time display bug

--- src/components/Timestamp/index.js
+++ src/components/Timestamp/index.js
@@ -1,16 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import { FlatList, Text } from 'react-native';
+import React, {useEffect, useState, useContext} from 'react';
+import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
 import { useTheme } from 'styled-components';
+import { AuthContext } from '../../routes/AuthProvider';
+
 import CustomButton from '../CustomButton';
 
-import { Container, CurrentDay, TimeItem } from './styles';
+import { 
+  ButtonsContainer, 
+  Container, 
+  CurrentDay, 
+  Separator, 
+  StopButtonsContainer, 
+  TimeItem 
+} from './styles';
 
 import { 
-  parseISO, 
   format, 
-  formatRelative, 
-  formatDistance,
-  getDayOfYear,
   getDate,
   getDay,
   getMonth,
@@ -18,54 +25,225 @@ import {
   getHours,
   getMinutes,
   getSeconds,
-  parse
 } from 'date-fns';
 
 import pt from 'date-fns/locale/pt-BR';
 
 const Timestamp = () => {
   const theme = useTheme();
-  const [timeList, setTimeList] = useState([]);
-  var date = new Date();
-  const day = getDate(date);
-  const weekDay = getDay(date);
-  const month = getMonth(date);
-  const year = getYear(date);
+  const { user, timeSheetRef } = useContext(AuthContext);
 
+  // const [type, setType] = useState('');
+  const [timeList, setTimeList] = useState([]);
+  // const myList = [];
+  const [isStarted, setIsStarted] = useState(false);
+  const [isLunch, setIsLunch] = useState(false);
+  const [isStopped, setIsStopped] = useState(false);
+  const [isFinishable, setIsFinishable] = useState(false);
+  var date = new Date();
+  // const day = getDate(date);
+  // const weekDay = getDay(date);
+  // const month = getMonth(date);
+  // const year = getYear(date);
+  const types = [
+    'Inicio de jornada',
+    'Fim de jornada',
+    'Almoço',
+    'Inicio de pausa',
+    'Retorno'
+  ]
   const formattedDate = format(
     date, 
     "EEEE, dd 'de' MMMM 'de' yyyy",
     {locale: pt}
   );
 
-  const formattedTime = format(
-    date, "kk:mm:ss"
-  )
-
   const renderItem = ({item}) => (
-    <TimeItem>{item}</TimeItem>
+    <TimeItem>
+      • {item.time} - {item.type}
+    </TimeItem>
   );
 
-  function handleButtonClick(){
-    setTimeList([...formattedTime, formattedTime])
+  function clearArray(arr) {
+    // timeSheetRef.doc(user.uid).set({data: arr}, {merge: true})
+    timeSheetRef.doc(user.uid).get().then((document) => {
+      if(document.exists) {
+        // set timestamp
+        timeSheetRef.doc(user.uid).update({
+          createdAt: firestore.FieldValue.serverTimestamp()
+        })
+        timeSheetRef.doc(user.uid).set({data: arr}, {merge: true})
+      }
+    });
+    setTimeList([]);
+    const newArr = arr.length = 0;
+    return newArr;
   }
 
- 
+  function handleStartClick(){
+    //setType(types[0])
+    setIsStarted(true)
+    pushList(types[0])
+    console.log('inicio jornada', timeList)
+  }
+  function handleFinishClick(){
+    //setType('Fim de jornada')
+    setIsStarted(false)
+    setIsFinishable(false)
+    setIsLunch(false)
+    setIsStopped(false)
+    pushList(types[1])
+    clearArray(timeList)
+    console.log('fim jornada', timeList)
+  }
+  function handleLunchClick(){
+    //setType('Almoço')
+    setIsLunch(true)
+    setIsFinishable(true)
+    pushList(types[2])
+    console.log('inicio almoço', timeList)
+  }
+  function handleStopClick(){
+    //setType('Inicio de pausa')
+    setIsStopped(true)
+    pushList(types[3])
+    console.log('inicio pausa', timeList)
+  }
+  function handleResumeClick(){
+    //setType('Retorno')
+    setIsLunch(false)
+    setIsStopped(false)
+    pushList(types[4])
+    console.log('fim pausa', timeList)
+  }
+
+  const formattedTime = format(
+    date, 'kk:mm:ss'
+  )
+
+
+  function pushList(typeName){
+    let newTimeList = {};
+    newTimeList.time = formattedTime;
+    newTimeList.type = typeName;
+    setTimeList((prev) => {
+      return [...prev, newTimeList]
+    })
+  }
+
+  function renderButtons(){
+    if(isStarted){
+      if (isLunch || isStopped){
+        return(
+          <ButtonsContainer>
+            <CustomButton 
+              style={{
+                width: 206, 
+                height: 82,
+                backgroundColor: theme.colors.primary,
+              }}
+              icon={{iconName: 'caret-left', iconSize: 15}}
+              buttonText={'Retornar'}
+              onPress={handleResumeClick}
+            />
+          </ButtonsContainer>
+        )
+      }
+      if (isFinishable){
+        return(
+          <ButtonsContainer>
+            <CustomButton 
+              style={{
+                width: 103, 
+                height: 82,
+                backgroundColor: theme.colors.bgSecundaryButton,
+                flexDirection: 'column',
+              }}
+              icon={{iconName: 'hand-stop-o', iconSize: 15, iconColor: theme.colors.defaultText}}
+              buttonText={'Pausa rápida'}
+              textColor={ theme.colors.defaultText }
+              onPress={handleStopClick}
+            />
+            <CustomButton 
+              style={{
+                width: 206, 
+                height: 82,
+                backgroundColor: theme.colors.primary,
+              }}
+              icon={{iconName: 'close', iconSize: 15}}
+              buttonText={'Finalizar jornada'}
+              onPress={handleFinishClick}
+            />
+          </ButtonsContainer>
+        )
+      }
+      return(
+        <ButtonsContainer>
+            <StopButtonsContainer>
+              <CustomButton 
+                style={{
+                  width: 103, 
+                  height: 82,
+                  backgroundColor: theme.colors.lunchButton,
+                  flexDirection: 'column',
+                }}
+                icon={{iconName: 'cutlery', iconSize: 15}}
+                buttonText={'Almoço'}
+                onPress={handleLunchClick}
+              />
+              <CustomButton 
+                style={{
+                  width: 103, 
+                  height: 82,
+                  backgroundColor: theme.colors.bgSecundaryButton,
+                  flexDirection: 'column',
+                }}
+                icon={{iconName: 'hand-stop-o', iconSize: 15, iconColor: theme.colors.defaultText}}
+                buttonText={'Pausa rápida'}
+                textColor={ theme.colors.defaultText }
+                onPress={handleStopClick}
+              />
+            </StopButtonsContainer>
+            <CustomButton 
+              style={{
+                width: 206, 
+                height: 82,
+                backgroundColor: theme.colors.primary,
+              }}
+              icon={{iconName: 'close', iconSize: 15}}
+              buttonText={'Finalizar jornada'}
+              onPress={handleFinishClick}
+            />
+          </ButtonsContainer>
+      )
+    } else {
+      return(
+        <ButtonsContainer>
+          <CustomButton 
+            style={{
+              width: 206, 
+              height: 82,
+              backgroundColor: theme.colors.primary,
+            }}
+            icon={{iconName: 'play', iconSize: 15}}
+            buttonText={'Iniciar jornada'}
+            onPress={handleStartClick}
+          />
+        </ButtonsContainer>
+      )
+    }
+  }
 
   return (
     <Container>
       <CurrentDay>{formattedDate}</CurrentDay>
-      <FlatList data={timeList} renderItem={renderItem}/>
-      <CustomButton 
-        style={{
-          width: 206, 
-          height: 82,
-          backgroundColor: theme.colors.primary,
-        }}
-        icon={{iconName: 'play', iconSize: 15}}
-        buttonText={'Iniciar jornada'}
-        onPress={handleButtonClick}
+      <FlatList 
+        data={timeList} 
+        renderItem={renderItem}
+        ItemSeparatorComponent={() => <Separator/>}
       />
+        {renderButtons()}
+        
     </Container>
   );
 }
--- src/components/Timestamp/styles.js
+++ src/components/Timestamp/styles.js
@@ -1,19 +1,41 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
-  flex: 1;
+  flex: 0.8;
+  /* align-items: center; */
+  /* justify-content: center; */
+  /* background-color: ${({theme}) => theme.colors.secundaryText}; */
+  `;
+
+export const ButtonsContainer = styled.SafeAreaView`
   align-items: center;
   justify-content: center;
-  background-color: #777;
+  /* background-color: '#f00'; */
+`;
+
+export const StopButtonsContainer = styled.SafeAreaView`
+  flex-direction: row;
+  width: 235px;
+  align-items: center;
+  justify-content: space-between;
+  /* background-color: #777; */
 `;
 
 export const CurrentDay = styled.Text`
+  font-family: ${({theme}) => theme.fonts.light};
   font-size: 18px;
   color: #000;
+  margin: 10px;
+  border-bottom-width: 1px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background-color: ${({theme}) => theme.colors.secundary};
 `;
 
 export const TimeItem = styled.Text`
-  flex-direction: row;
-  /* align-items: center;
-  justify-content: center; */
+  font-family: ${({theme}) => theme.fonts.rajSemiBold};
+  font-size: 18px;
+  margin-left: 10px;
 `;
